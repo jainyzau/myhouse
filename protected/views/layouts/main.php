9bbf08ee3ee3e2ff4ed77384ef1788bdfdd0acde
Jainy Zau <jainyzau@gmail.com>
2014-07-17 12:01:29 -0400

    Changed index page style; Added carousel on index page.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -86,8 +86,6 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
     <?php
       Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . "/js/jquery.min.js", CClientScript::POS_END);
       Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . "/js/bootstrap.min.js", CClientScript::POS_END);
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,8 +1,57 @@
+
+  <div>
+    <p></p>
+  </div>
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="width:970px;">
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox" >
+      <div class="item active">
+        <img src="/myhouse/images/slides001.jpg" alt="First slide">
+      </div>
+      <div class="item">
+        <img src="/myhouse/images/slides002.jpg" alt="Second slide">
+      </div>
+      <div class="item">
+        <img src="/myhouse/images/slides003.jpg" alt="Third slide">
+      </div>
+      <div class="item">
+        <img src="/myhouse/images/slides004.jpg" alt="Fourth slide">
+      </div>
+    </div>
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 <?php
 /* @var $this SiteController */
 Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . "/js/jqueryui/jquery-ui.min.js", CClientScript::POS_END);
+Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . "/js/docs.min.js", CClientScript::POS_END);
 $this->pageTitle=Yii::app()->name;
 ?>
+    <div>
+      <p></p>
+    </div>
+      <div class="row">
+        <div class="col-sm-10">
+          <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
+      </div>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
@@ -18,3 +67,4 @@ $this->pageTitle=Yii::app()->name;
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
 should you have any questions.</p>
+
